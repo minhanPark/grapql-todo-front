5cade1e841d425b9c1d0eca2aa1f6261dcc0b14d
박민한 <alsgks1218@naver.com>
2021-07-29 10:38:48 +0900

    add global style and test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { GlobalStyle } from "./style/Common";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 60px;
+  color: red;
+`;
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div className="App">
+        <Title>sdsdsdsdsdsd</Title>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
